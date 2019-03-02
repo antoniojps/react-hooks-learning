f8e43c1a423d9cee8c1275fd0cb93ffbf7bd74b8
António Santos <antoniojps@ua.pt>
2019-03-02 21:53:48 +0000

    Add Toggle component and example formSubmit func

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'
+import Toggle from './Toggle'
 
 const App = () => {
+  // const [value, setValue] = useState(initialState)
+  const [name, setName] = useState('')
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          formSubmit(name, setName)
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+const formSubmit = (value, setValue) => {
+  console.log(`Email sent to ${value}!`)
+  setValue('')
+}
+
+export default App
