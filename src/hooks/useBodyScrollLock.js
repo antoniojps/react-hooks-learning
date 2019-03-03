c9d719aad5c41434612fc1108d53ab25a0963654
António Santos <antoniojps@ua.pt>
2019-03-03 21:31:13 +0000

    clean up function for useLayoutEffect and useEffect

--- src/hooks/useBodyScrollLock.js
+++ src/hooks/useBodyScrollLock.js
@@ -2,7 +2,11 @@ import { useLayoutEffect } from 'react'
 
 function useBodyScrollLock() {
   useLayoutEffect(() => {
+    const originalValue = window.getComputedStyle(document.body).overflow
     document.body.style.overflow = 'hidden'
+    // clean up function, runs on unmount
+    // works with useLayoutEffect or useEffect
+    return () => (document.body.style.overflow = originalValue)
   })
 }
 
