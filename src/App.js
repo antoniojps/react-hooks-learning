821cd731513368761be10e5dff01c59362f6b869
António Santos <antoniojps@ua.pt>
2019-03-03 18:34:55 +0000

    Remove Context, Toggle, Counter from App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
-import React, { useRef, createContext } from 'react'
+import React, { useRef } from 'react'
 import { useTitleInput } from './hooks/useTitleInput'
-import Toggle from './Toggle'
-import Counter from './Counter'
-
-export const UserContext = createContext()
 
 const App = () => {
   const [name, setName] = useTitleInput('test')
   const ref = useRef()
 
   return (
-    <UserContext.Provider
-      value={{
-        user: false
-      }}
-    >
-      <div className="main-wrapper" ref={ref}>
-        <h1>Level Up Dishes</h1>
-        <Toggle />
-        <Counter />
-        <button onClick={() => console.log(ref.current.className)}>
-          Check ref
-        </button>
-        <form
-          onSubmit={e => {
-            e.preventDefault()
-          }}
-        >
-          <input
-            type="text"
-            onChange={e => setName(e.target.value)}
-            value={name}
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
-    </UserContext.Provider>
+    <div className="main-wrapper" ref={ref}>
+      <h1>Level Up Dishes</h1>
+      <button onClick={() => console.log(ref.current.className)}>
+        Check ref
+      </button>
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+        }}
+      >
+        <input
+          type="text"
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
 
